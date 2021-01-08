aaf92e2dd40d76dd1fcf68d35840b8e6b34a25c3
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2021-01-08 23:21:19 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/ujianptm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ujianptm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/FindElephant.dart
@@ -0,0 +1,227 @@
+import 'package:ujianptm/FadeAnimation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:ujianptm/Model/Elephant.dart';
+import 'dart:convert';
+
+import 'Detail.dart';
+import 'Model/Elephant.dart';
+
+class FindGame extends StatefulWidget {
+  @override
+  _FindGameState createState() => _FindGameState();
+}
+
+class _FindGameState extends State<FindGame> {
+  List<Elephant> _elephantList = [];
+
+  Future<List<Elephant>> get getElephant async {
+    final resp =
+        await http.get("https://elephant-api.herokuapp.com/elephants/sex/male");
+    if (resp.statusCode != 200) {
+      throw ("gagal Terhubung dengan server (Code: ${resp.statusCode}");
+    }
+    final dynamic res = jsonDecode(resp.body);
+
+    if (resp.statusCode == 200) {
+      res.forEach((dynamic data) {
+        _elephantList.add(Elephant.fromJson(jsonEncode(data)));
+      });
+      return _elephantList;
+    }
+  }
+
+  @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(246, 248, 253, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        brightness: Brightness.light,
+        leading: Icon(
+          Icons.menu,
+          color: Colors.black,
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Container(
+              width: 35,
+              height: 35,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: AssetImage('assets/images/elephant-api-logo.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Transform.translate(
+                offset: Offset(15, -15),
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 3, color: Colors.white),
+                    shape: BoxShape.circle,
+                    color: Colors.deepOrange[800],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // FadeAnimation(
+              //     1,
+              //     Container(
+              //       padding: EdgeInsets.symmetric(vertical: 5),
+              //       decoration: BoxDecoration(
+              //           borderRadius: BorderRadius.circular(50),
+              //           color: Colors.white),
+              //       child: TextField(
+              //         decoration: InputDecoration(
+              //             border: InputBorder.none,
+              //             prefixIcon: Icon(
+              //               Icons.search,
+              //               color: Colors.grey,
+              //             ),
+              //             hintText: "Search Game",
+              //             hintStyle: TextStyle(color: Colors.grey)),
+              //       ),
+              //     )),
+              // SizedBox(
+              //   height: 30,
+              // ),
+              Expanded(
+                child: FutureBuilder<List<Elephant>>(
+                  initialData: _elephantList,
+                  future: getElephant,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      if (snapshot.hasData &&
+                          snapshot.connectionState == ConnectionState.done &&
+                          snapshot.data.length > 0) {
+                        return ListView.builder(
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (context, index) {
+                            Elephant _modelElephant = snapshot.data[index];
+                            return Container(
+                              child: GestureDetector(
+                                onTap: () => Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Detail(
+                                        dataDetail: _modelElephant,
+                                      ),
+                                    )),
+                                child: Column(
+                                  children: [
+                                    FadeAnimation(
+                                      1.5,
+                                      makeItem(
+                                        image: _modelElephant.image,
+                                        title: _modelElephant.name,
+                                        genre: _modelElephant.sex +
+                                            " - " +
+                                            _modelElephant.species,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 20,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      } else {
+                        print(snapshot.data.length);
+                        return Container();
+                      }
+                    } else {
+                      return Container(
+                        width: 90,
+                        height: 90,
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget makeItem({image, title, genre}) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            height: 200,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                    image: NetworkImage(image), fit: BoxFit.cover)),
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(colors: [
+                    Colors.black.withOpacity(.4),
+                    Colors.black.withOpacity(.1),
+                  ])),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(
+                    title,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.airplay_rounded,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        genre,
+                        style: TextStyle(color: Colors.white),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
